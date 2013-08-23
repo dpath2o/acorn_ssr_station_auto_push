b01b3d3fd05423a44ed872e02cf563787a720ca3
dpath2o <danielpath2o@gmail.com>
2013-08-23 17:35:32 +1000

    updated seasonde_station_data_transfer_push.pl

--- perl/seasonde_station_data_transfer_push.pl
+++ perl/seasonde_station_data_transfer_push.pl
@@ -1,4 +1,4 @@
-#!/opt/local/bin/perl
+#!/usr/bin/perl
 #
 # seasonde_station_data_transfer_push.pl
 #
@@ -6,30 +6,26 @@
 # to a remote server.
 #
 # This file uses HFR::FileTransfer module and it's associated modules. I encourage you to read the
-# documentation on this module to understand what this script is doing or before modifying this script
+# documentation on this module to understand what that module is doing or before modifying this script
 # to suit your needs.
-#
-# Author: Daniel Patrick Atwater
-#         Australia Coastal Ocan Radar Network
-#         James Cook University
-#         email: danielpath2o@gmail.com
-#
-# Copyright: Open source software, May 2012
 
+################################################################################
+# LOAD MODULES/LIBRARIES
 use strict;
 use DateTime;
-use HFR::FileTransfer;
 use Getopt::Long;
+use Pod::Usage;
 use Log::LogLite;
-use Term::ANSIColor qw(:constants);
 use Net::NSCA::Client;
+use HFR::FileTransfer;
 
-# get datetime
+################################################################################
+# LOAD CURRENT SYSTEM TIME
 my $dt = DateTime->now;
 
-# initialise and set defaults
-#my ($sf,$rh,$dd,$station);
-my ($sf,@rh,@dd,$station);
+################################################################################
+# INITIALISE INPUTS
+my ($sf,$rh,$dd,$station);
 my $ad        = '/codar/seasonde/archives';
 my $sd        = '/codar/seasonde/data/radials/measpattern';
 my $qd        = "$ENV{HOME}/queued";
@@ -44,14 +40,15 @@ my $verbose   = 0;
 my $debug     = 0;
 my $help_msg  = 0;
 
-# get input options
+################################################################################
+# GET INPUTS
 my $result = GetOptions (
 			 "sf|source_file=s"           => \$sf,
 			 "sd|source_directory=s"      => \$sd,
 			 "qd|queued_directory=s"      => \$qd,
 			 "ad|archive_directory=s"     => \$ad,
-			 "rh|remote_host=s"           => \@rh,
-			 "dd|destination_directory=s" => \@dd,
+			 "rh|remote_host=s"           => \$rh,
+			 "dd|destination_directory=s" => \$dd,
 			 "ssh_key_file=s"             => \$ssh_key,
 			 "logit"                      => \$logit,
 			 "log_file=s"                 => \$log_file,
@@ -63,41 +60,39 @@ my $result = GetOptions (
 			 "d|debug"                    => \$debug,
                          "h|help!"                    => \$help_msg) or usage("Invalid commmand line options.");
 
-# print usage and exit
-usage("Undefined remote host input argument\n\n") if ( !@rh );
-usage("Undefined destination directory input argument\n\n") if ( !@dd );
+################################################################################
+# PRINT USAGE AND EXIT
 usage("HELP MESSAGE:") if ( $help_msg==1 );
 
-# open the log file
+################################################################################
+# OPEN LOG
 my $log = new Log::LogLite( $log_file , $log_level );
 
-# single file transfer or transfer of queued files
+################################################################################
+# PRIMARY CONDITION: queued or not single file transfer
 if ( $queued==1 ) {
 
   # exit here if any of the directories do  not exist
   if ( !(-d $sd) ) {
 
-    my $msg = "Source directory :: $sd\nEither undefined as an input argument OR the directory itself does not exist ... \nexiting $0\n";
+    my $msg = "Source directory :: $sd\nEither undefined as an input argument OR the directory itself does not exist ... \nERROR! EXITING PROGRAM $0\n";
     $log->write( $msg , $log_level ) if ($logit);
-    err( $msg ) if ($verbose);
     exit;
 
   }
 
   if ( !(-d $qd) ) {
 
-    my $msg = "Queued directory :: $qd\nEither undefined as an input argument OR the directory itself does not exist ... \nexiting $0\n";
+    my $msg = "Queued directory :: $qd\nEither undefined as an input argument OR the directory itself does not exist ... \nERROR! EXITING PROGRAM $0\n";
     $log->write( $msg , $log_level ) if ($logit);
-    err( $msg ) if ($verbose);
     exit;
 
   }
 
   if ( !(-d $ad) ) {
 
-    my $msg = "Archive directory :: $ad\nEither undefined as an input argument OR the directory itself does not exist ... \nexiting $0\n";
+    my $msg = "Archive directory :: $ad\nEither undefined as an input argument OR the directory itself does not exist ... \nERROR! EXITING PROGRAM $0\n";
     $log->write( $msg , $log_level ) if ($logit);
-    err( $msg ) if ($verbose);
     exit;
 
   }
@@ -106,8 +101,8 @@ if ( $queued==1 ) {
 					     source_directory      => $sd,
 					     queued_directory      => $qd,
 					     archive_directory     => $ad,
-                                             remote_host           => \@rh,
-                                             destination_directory => \@dd,
+                                             remote_host           => $rh,
+                                             destination_directory => $dd,
 					     ssh_key_file          => $ssh_key,
 					     log_file              => $log_file,
 					     logit                 => $logit,
@@ -125,17 +120,16 @@ if ( $queued==1 ) {
   # the primary cross spectra to radial processing
   if ( !(-e $sf) ) {
 
-    my $msg = "Source file :: $sf\nEither undefined as an input argument OR the file itself does not exist ... \nexiting $0\n";
+    my $msg = "Source file :: $sf\nEither undefined as an input argument OR the file itself does not exist ... \nERROR! EXITING PROGRAM $0\n";
     $log->write( $msg , 6 ) if ($logit);
-    err( $msg ) if ($verbose);
     exit;
 
   }
 
   my $xfer = HFR::FileTransfer->new_transfer(
 					     source_file           => $sf,
-                                             remote_host           => \@rh,
-                                             destination_directory => \@dd,
+                                             remote_host           => $rh,
+                                             destination_directory => $dd,
 					     ssh_key_file          => $ssh_key,
 					     log_file              => $log_file,
 					     logit                 => $logit,
@@ -163,72 +157,133 @@ if ( $queued==1 ) {
 
   }
 }
+__END__
 
-#####################################################
-sub err {
+=head1 NAME
 
-  my $self = shift;
-  my $msg  = shift;
-  print RED, $msg;
-  print RESET;
+This script is intended to 'push' SeaSonde data from a SeaSonde station to a server.
 
-}
+=head1 SYNOPSIS
 
-#####################################################
-sub usage {
+seasonde_station_data_transfer_push.pl --remote_host=<some.server.com> --destination_directory='/remote/data/directory' [options]
 
-  # print input message if any
-   my $message = $_[0];
-   if (defined $message && length $message) {
-     $message .= "\n" unless $message =~ /\n$/;
-   }
+For all default options please see L<HFR::YAML>.
 
-   # name of the script
-   my $command = $0;
-   $command =~ s#^.*/##;
+=head2 REQUIRED INPUTS
 
-   print STDERR (
-      $message,
-      "usage: $command\n".
-      "   This script is called in one of two ways:\n".
-      "      1.) single file transfer\n".
-      "      2.) queued (multiple or single) file transfer\n".
-      "      When called with as a single file transfer then here is a simple example of how it might be called:\n".
-      "         ./seasonde_station_data_transfer_push.pl --sf='/codar/seasonde/data/radials/measpattern/RDLm_BFCV_2012_01_17_0900.ruv' --rh='foo.com' --dd='/foo/incoming/bfcv' -v\n".
-      "      Whereas if it were being called for a multiple file transfer the call would be something like:\n".
-      "         ./seasonde_station_data_transfer_push.pl --queued --sd='/codar/seasonde/data/radials/measpattern' --rh='foo.com' --dd='/foo/incoming/bfcv' -v\n\n".
-      "      The difference between the single and queued call methods is that the single can be thought of as an explicit way to transfer one file\n".
-      "      whereas the queued file method searches for files that have the suffix '.queued' in a queued directory, then attempts to find those files in\n".
-      "      in either the 'source directory' or in an 'archive directory'.\n".
-      "      The script calls the perl module HFR::FileTransfer to do the work of transferring.\n".
-      "      Please see 'perldoc HFR::FileTransfer' for more information, and if you don't have a copy of that module then please email danielpath2o<at>gmail.com\n\n".
-      "      \nScript uses Net::NSCA::Client for notification of icinga remote monitoring of a successful file transmission\n".
-      "          REQUIRED INPUTS:\n".
-      "               --rh|remote_host='seasonde.jcu.edu.au'\n".
-      "               --dd|destination_directory='/volumes/data/incoming/bfcv'\n".
-      "          OPTIONAL INPUTS:\n".
-      "               [--sf|source_file='/codar/seasonde/data/radials/measpattern/rdlm_bfcv_2010_09_30_0000.ruv']\n".
-      "               [--sd|source_directory='/codar/seasonde/data/radials/measpattern']\n".
-      "               [--ssh_key_file='$ENV{HOME}/.ssh/id_rsa\n".
-      "               [--logit]\n".
-      "               [--log-file='/codar/seasonde/logs/filetransfer.log']".
-      "               [--queued]\n".
-      "               [--qd|queued_directory='$ENV{HOME}/queued']\n".
-      "               [--ad|archive_directory='/codar/seasonde/archives']\n".
-      "               [--user='codar']\n".
-      "               [-v] [-d] [-h]\n\n".
-      "       sf     :: source full path of file to transfer (single file transfer is assumed)\n" .
-      "       sd     :: source directory of the file; default is '/codar/seasonde/data/radials/measpattern'; this is critical when enabling/attempting to transfer queued files\n".
-      "       rh     :: remote hostname\n" .
-      "       dd     :: destination directory\n".
-      "       ad     :: local archive directory; default is '/codar/seasonde/archives'\n".
-      "       ssh_key:: SSH RSA key file; default is '$ENV{HOME}/.ssh/id_rsa'\n".
-      "       qd     :: queued directory; default is '$ENV{HOME}/queued'\n".
-      "       user   :: name of user; default is 'codar'\n".
-      "       queued :: turn on queued file transfer\n".
-      "       d      :: prevents any copying or moving files; default behaviour: debug on\n".
-      "       v      :: verbose\n" .
-      "       h      :: print this message and exit\n".
-      "\n");
-   die("\n")
- }
+=over 8
+
+=item B<rh|remote_host>
+
+Hostname or IP address of remote host/server to push files onto
+
+=item B<dd|destination_directory>
+
+Directory on remote host/server to push files into
+
+=cut
+
+=head2 OPTIONAL INPUTS
+
+=over 8
+
+=item B<sf|source_file>
+
+Full path or just file name of a single file to push
+
+=item B<sd|source_directory>
+
+Path where file(s) exists
+
+=item B<ssh_key_file>
+
+The SSH key file (full path and filename) for automated transfers.  This script and associated modules will not prompt for a password and will terminate if password request is encountered.
+
+=item B<user>
+
+Give a username to pass to SSH if logging in as someone other than the user running this script.
+
+=item B<logit>
+
+Boolean turns on/off logging
+
+=item B<log-file>
+
+Full path and filename of the log file.
+
+=item B<queued>
+
+Boolean turns on/off queuing
+
+=item B<qd|queued_directory>
+
+Full path to directory to list queued files
+
+=item B<ad|archive_directroy>
+
+Full path to directory that contains archives files.  This is for when attempting to push a queued filename that is no longer in its source directory, because its been archived.
+
+=item B<v|verbose>
+
+Messages sent to STDOUT as well
+
+=item B<d|debug>
+
+Debug: don't actually transfer or unlink anything
+
+=item B<h|help>
+
+Print this usage message and exit
+
+=back
+
+=head1 REQUIREMENTS
+
+=over 8
+
+=item DateTime
+
+See L<DateTime>
+
+=item Getopt::Long
+
+See L<Getopt::Long>
+
+=item Pod::Usage
+
+See L<Pod::Usage>
+
+=item Log::LogLite
+
+See L<Log::LogLite>
+
+=item Net::NSCA::Client
+
+See L<Net::NSCA::Client>
+
+=item HFR::FileTransfer
+
+See L<HFR::FileTransfer>
+
+=back
+
+=head1 DESCRIPTION
+
+   This script is called in one of two ways:
+      1.) single file transfer
+      2.) queued (multiple or single) file transfer
+      When called with as a single file transfer then here is a simple example of how it might be called:
+         ./seasonde_station_data_transfer_push.pl --sf='/codar/seasonde/data/radials/measpattern/RDLm_BFCV_2012_01_17_0900.ruv' --rh='foo.com' --dd='/foo/incoming/bfcv' -v\n".
+      Whereas if it were being called for a multiple file transfer the call would be something like:
+         ./seasonde_station_data_transfer_push.pl --queued --sd='/codar/seasonde/data/radials/measpattern' --rh='foo.com' --dd='/foo/incoming/bfcv' -v\n\n".
+      The difference between the single and queued call methods is that the single can be thought of as an explicit way to transfer one file
+      whereas the queued file method searches for files that have the suffix '.queued' in a queued directory, then attempts to find those files in
+      in either the 'source directory' or in an 'archive directory'
+
+      The script calls the perl module HFR::FileTransfer to do the work of transferring
+
+      Please see 'perldoc HFR::FileTransfer' for more information
+
+      Script uses Net::NSCA::Client for notification of icinga remote monitoring of a successful file transmission
+
+=cut
